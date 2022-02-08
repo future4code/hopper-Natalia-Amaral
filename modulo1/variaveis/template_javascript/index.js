ed324740d03e42b2bcb61fedbec48bafdd46b0f3
unknown <nataliaheloisa95@gmail.com>
2022-02-08 19:28:46 -0300

    excercício-javascript

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,32 @@
+//Declare uma variável para armazenar um nome, sem atribuir um valor
+// let nome = prompt (" Qual é o seu nome? ")
+
+// // Declare uma variável para armazenar uma idade, sem atribuir um valor
+// let idade = prompt (" Qual sua idade? ")
+
+// // Imprima na tela o tipo dessas variáveis que acabou de criar, usando o comando typeof
+// console.log = (typeof nome)
+// console.log = (typeof idade)
+
+// Para finalizar, imprima na tela a mensagem: "Olá nome,  você tem idade anos". Onde nome e idade são os valores que o usuário inseriu
+// console.log("Ola meu nome e Natalia tenho 26 anos")
+
+// Escreva um programa que faça 3 perguntas de Sim ou Não, armazenado em uma variável. Por exemplo: "Você está usando uma roupa azul hoje?". Depois, siga os passos:
+// let azulBoolean = false
+// azulBoolean = false
+// let rosaboolean = true
+// rosaBoolean = true
+// let pretoBoolean = false
+// pretoBoolean = false
+let a = 10
+let b = 25
+// Aqui faremos uma lógica para trocar os valores
+// Depois de trocados, teremos o seguinte resultado:
+// console.log("O novo valor de a é" , 25)
+// console.log("O novo valor de b é", 10)
+// a = b
+// b = a
+// c = 10
+// a = c 
+// b = a 
+// c = b
