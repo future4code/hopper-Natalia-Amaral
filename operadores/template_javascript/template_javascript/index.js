2adf1059414297bd7382a22ac1d718f676a18147
unknown <nataliaheloisa95@gmail.com>
2022-02-10 22:28:58 -0300

    Exercícios com operadores

---
+++ operadores/template_javascript/template_javascript/index.js
@@ -0,0 +1,89 @@
+//Declare uma variável para armazenar um nome, sem atribuir um valor
+// let nome = prompt (" Qual é o seu nome? ")
+
+// // Declare uma variável para armazenar uma idade, sem atribuir um valor
+// let idade = prompt (" Qual sua idade? ")
+
+// // Imprima na tela o tipo dessas variáveis que acabou de criar, usando o comando typeof
+// console.log = (typeof nome)
+// console.log = (typeof idade)
+
+// Para finalizar, imprima na tela a mensagem: "Olá nome,  você tem idade anos". Onde nome e idade são os valores que o usuário inseriu
+// console.log("Ola meu nome e Natalia tenho 26 anos")
+
+// Escreva um programa que faça 3 perguntas de Sim ou Não, armazenado em uma variável. Por exemplo: "Você está usando uma roupa azul hoje?". Depois, siga os passos:
+// deixa 
+
+// let a = 10
+// let b = 25
+// Aqui faremos uma lógica para trocar os valores
+// Depois de trocados, teremos o seguinte resultado:
+// console.log("O novo valor de a é" , 25)
+// console.log("O novo valor de b é", 10)
+// a = b
+// b = a
+// c = 10
+// a = c 
+// b = a 
+// c = b
+
+
+// Interpretação de código:
+// a. False  
+// b. False 
+// c. True
+// d. Boolean
+
+// Ajudando o amigo a resolver seu problema do código:
+// let primeiroNumero = prompt("Digite um numero!")
+// let segundoNumero = prompt("Digite outro numero!")
+// const resultado = Number(primeiroNumero) + Number(segundoNumero)
+// console.log(resultado) // Eu sugiro converter para Number, pois o prompt sempre vai dar uma string.
+// E nesse caso iria juntar os numeros e não somar.
+
+// 1- Faça um programa que:
+// const idadeUsuario = prompt("Qual sua idade ?")
+// const idadeAmigo = prompt("Qual a idade de seu melhor amigo?")
+// console.log("Sua idade é maior do que a de seu amigo?")
+// const resultado = (!true || false)
+// console.log(resultado)
+// const idadeUsuario = 26
+// const idadeAmigo = 41
+// const resultado = (26-41) 
+// console.log(resultado) //-15
+
+// 2- Faça um programa que:
+// const numeroPar = prompt ("Insira um número par!")
+// const numeroPar2 = 10
+// const resultado = (10/2)
+// console.log(resultado) // 5
+// const Par1 = 110
+// const Par2 = 120
+// const Par3 = 160
+// const resultado = 110 / 120/ 160 / 2
+// console.log(resultado) // 0,0028645833333333.....
+// const numeroImpar = prompt ("Insira um numero")
+// const numeroImpar2 = 15
+// const resultado = (15/2)
+// console.log(resultado) // 7,5 - Caso o usuário insira um número ímpar o resultado da quebrado. 
+
+// 3- Faça um programa que pergunte ao usuário sua idade em anos. Depois, imprima no console
+// let minhaidade = prompt("Qual sua idade?") // 26
+// console.log ("Sua idade em meses é de:", minhaidade * 12, "meses")
+// console.log ("Sua idade em dias é de:", minhaidade * 365, "dias")
+// console.log ("Sua idades em horas é de:", minhaidade * 8760, "horas")
+
+// 4- Faça um programa que pergunte ao usuário dois números.
+// Em seguida, faça as operações e imprima no console as seguintes mensagens seguidas pelo true ou false:
+// const numero1 = prompt("Digite um numero!")
+// const numero2 = prompt("Digite outro numero!")
+// const numero1 = 4
+// const numero2 = 2
+// const resultado = (4 > 2 ) // True
+// console.log(resultado)
+// const resultado = (4 === 2) // False
+// console.log(resultado)
+// const resultado = (4 % 2 ===0) // True
+// console.log(resultado)
+// const resultado = (2 % 4 === 0) // False
+// console.log(resultado)
