2fbc4c9ce8edf239f7e215c68f3aa66ed01a5042
nataliaheloisa <nataliaheloisa95@gmail.com>
2022-04-28 01:14:10 -0300

    Reinderizando site.

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import Post from './components/Post/Post';
 
 const MainContainer = styled.div`
@@ -9,8 +9,92 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+const Button = styled.button`
+  text-transform: uppercase;
+  background-color: gray;
+  color: black;
+  height: 3vh;
+  width: 100%;
+  border-style: 2px 2px 2px 2px solid;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Input = styled.input`
+  flex-direction: row;
+  text-align:center;
+  color: black;
+  background-color: pink;
+`;
+
+
+
+
+
 class App extends React.Component {
+
+  state = {
+    internautas: [
+      {
+        nomeUsuario: "Paulinha",
+        fotoUsuario: "bla",
+        fotoPost: "bla"
+      },
+      {
+        nomeUsuario: "Aninha",
+        fotoUsuario: "bla",
+        fotoPost: "bla"
+      },
+      {
+        nomeUsuario: "Joana",
+        fotoUsuario: "bla",
+        fotoPost: "bla"
+      }
+
+    ],
+
+    valorInputInternauta:"",
+    valorInputFoto:"",
+    valorInputPost:""
+
+  };
+
+  adicionaInternauta = () => {
+    let novoInternauta = {
+      nomeUsuario: this.state.valorInputInternauta,
+      fotoUsuario: this.state.valorInputFoto,
+      fotoPost: this.state.valorInputPost
+    };
+
+    let novoInternautas = [...this.state.internautas, novoInternauta];
+    this.setState({internautas:novoInternautas});
+  };
+
+  onChangeInputUsuario = (event) =>{
+    this.setState({valorInputInternauta: event.target.value});
+  };
+
+  onChangeInputFoto = (event) =>{
+    this.setState({valorInputFoto: event.target.value});
+  };
+
+  onChangeInputPost = (event) =>{
+    this.setState({valorInputPost: event.target.value});
+  };
+
+
   render() {
+
+    const listaUsuario = this.state.internautas.map((internautas) => {
+      return(
+        <p>
+          {internautas.nomeUsuario} , {internautas.fotoUsuario} , {internautas.fotoPost}
+        </p>
+      )
+    });
+    
     return (
       <MainContainer>
 
@@ -31,6 +115,26 @@ class App extends React.Component {
         fotoUsuario={'https://picsum.photos/50/52'}
         fotoPost={'https://picsum.photos/200/152'}
         />
+
+      <div>
+        <Input
+          value={this.state.valorInputInternauta}
+          onChange={this.onChangeInputInternauta}
+          placeholder={"Nome"}/>
+
+        <Input
+          value={this.state.valorInputFoto}
+          onChange={this.onChangeInputFoto}
+          placeholder={"Foto Perfil"}/>
+          
+
+        <Input
+          value={this.state.valorInputPost}
+          onChange={this.onChangeInputPost}
+          placeholder={"Post"}/>
+
+        <Button onClick={this.adicionaUsuario}><strong>Adicionar</strong></Button>
+      </div>
 
       </MainContainer>
     );
