98f2ffd2ba00f63e62d1f1c46adef3cfbfebe0fd
nataliaheloisa <nataliaheloisa95@gmail.com>
2022-08-16 22:52:20 -0300

    Intro-Express

---
+++ modulo5/Bloco14/intro-express/src/data.ts
@@ -0,0 +1,31 @@
+type Dados = {
+    id: number | string,
+    name: string,
+    phone: string,
+    email: string,
+    website: string,
+}
+
+export const users: Dados[] = [
+    {
+        id: 1,
+        name: "Fulanin",
+        email: "fulanin@fulanin.com",
+        phone: "993051111",
+        website: "fulanin.org",
+    },
+    {
+        id: 2,
+        name: "Ciclanin",
+        email: "ciclanin@ciclanin.com",
+        phone: "993061111",
+        website: "ciclanin.org",
+    },
+    {
+        id: 3,
+        name: "Beltranin",
+        email: "beltranin@beltranin.com",
+        phone: "993041111",
+        website: "beltranin.org",
+    },
+]
---
+++ modulo5/Bloco14/intro-express/src/index.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import { users } from "./data";
+import { posts } from "./posts";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {          
+    const port = 3003
+    const server = app.listen(port, () => {
+      if (server) {
+            console.log(`Server is running in http://localhost:${port}`);
+      } else {
+        console.error(`Failure upon starting server.`);
+      }
+    });
+    return res.status(200).json({message:"Hello World!!"})
+})
+
+app.get("/users", (req, res) => {          
+    const userData = users.map((user) =>{
+        return user
+    })
+    return res.status(200).send(userData)
+    
+})
+
+
+app.get("/posts", (req, res) => {          
+    const postData = posts.map((post) =>{
+        return post
+    })
+    return res.status(200).send(postData)
+    
+})
+
+app.get("/users/posts/:id", (req, res) => {
+    const id = req.params.id
+    const postsUser = posts.filter((post) => {
+        return post.userId === Number(id)
+    })
+    .map((res)=> {
+        return res
+    })
+    return res.status(200).send(postsUser) 
+})
